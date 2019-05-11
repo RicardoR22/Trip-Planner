a76876aae3549d36da4ac2f80861797ab233932a
Ricardo Rodriguez <rr092293@gmail.com>
2019-05-11 10:24:40 -0700

    first commit

---
+++ Trip Planner/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Trip Planner
+//
+//  Created by Ricardo Rodriguez on 5/7/19.
+//  Copyright © 2019 Ricardo Rodriguez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
